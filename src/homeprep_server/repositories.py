dad853364af1c11d9a004c1ef8153a70208f6a5d
kakelakel <32018143+kakelakel@users.noreply.github.com>
2026-09-14 19:08:15 +0200

    Add Asset and Task repositories

--- src/homeprep_server/repositories.py
+++ src/homeprep_server/repositories.py
@@ -3,7 +3,13 @@ from collections.abc import Sequence
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from homeprep_server.models import ContainerModel, HouseholdModel, InventoryItemModel
+from homeprep_server.models import (
+    AssetModel,
+    ContainerModel,
+    HouseholdModel,
+    InventoryItemModel,
+    TaskModel,
+)
 
 
 class HouseholdRepository:
@@ -57,6 +63,64 @@ class ContainerRepository:
                 ContainerModel.deleted_at.is_(None),
             )
             .order_by(ContainerModel.name.asc(), ContainerModel.id.asc())
+        )
+        return self.session.scalars(statement).all()
+
+
+class AssetRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, asset: AssetModel) -> AssetModel:
+        self.session.add(asset)
+        self.session.flush()
+        self.session.refresh(asset)
+        return asset
+
+    def get(self, asset_id: str) -> AssetModel | None:
+        statement = select(AssetModel).where(
+            AssetModel.id == asset_id,
+            AssetModel.deleted_at.is_(None),
+        )
+        return self.session.scalar(statement)
+
+    def list_for_household(self, household_id: str) -> Sequence[AssetModel]:
+        statement = (
+            select(AssetModel)
+            .where(
+                AssetModel.household_id == household_id,
+                AssetModel.deleted_at.is_(None),
+            )
+            .order_by(AssetModel.name.asc(), AssetModel.id.asc())
+        )
+        return self.session.scalars(statement).all()
+
+
+class TaskRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, task: TaskModel) -> TaskModel:
+        self.session.add(task)
+        self.session.flush()
+        self.session.refresh(task)
+        return task
+
+    def get(self, task_id: str) -> TaskModel | None:
+        statement = select(TaskModel).where(
+            TaskModel.id == task_id,
+            TaskModel.deleted_at.is_(None),
+        )
+        return self.session.scalar(statement)
+
+    def list_for_household(self, household_id: str) -> Sequence[TaskModel]:
+        statement = (
+            select(TaskModel)
+            .where(
+                TaskModel.household_id == household_id,
+                TaskModel.deleted_at.is_(None),
+            )
+            .order_by(TaskModel.next_due_at.asc(), TaskModel.name.asc(), TaskModel.id.asc())
         )
         return self.session.scalars(statement).all()
 
