29760a0fcaf8d00e39046669612890f32d01c62f
kakelakel <32018143+kakelakel@users.noreply.github.com>
2026-09-14 19:03:02 +0200

    Add Container repository

--- src/homeprep_server/repositories.py
+++ src/homeprep_server/repositories.py
@@ -3,7 +3,7 @@ from collections.abc import Sequence
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from homeprep_server.models import HouseholdModel, InventoryItemModel
+from homeprep_server.models import ContainerModel, HouseholdModel, InventoryItemModel
 
 
 class HouseholdRepository:
@@ -24,11 +24,26 @@ class HouseholdRepository:
         return self.session.scalar(statement)
 
     def list_all(self) -> Sequence[HouseholdModel]:
-        statement = (
-            select(HouseholdModel)
-            .where(HouseholdModel.deleted_at.is_(None))
-            .order_by(HouseholdModel.name.asc(), HouseholdModel.id.asc())
-        )
+        statement = select(HouseholdModel).where(HouseholdModel.deleted_at.is_(None)).order_by(HouseholdModel.name.asc(), HouseholdModel.id.asc())
+        return self.session.scalars(statement).all()
+
+
+class ContainerRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, container: ContainerModel) -> ContainerModel:
+        self.session.add(container)
+        self.session.flush()
+        self.session.refresh(container)
+        return container
+
+    def get(self, container_id: str) -> ContainerModel | None:
+        statement = select(ContainerModel).where(ContainerModel.id == container_id, ContainerModel.deleted_at.is_(None))
+        return self.session.scalar(statement)
+
+    def list_for_household(self, household_id: str) -> Sequence[ContainerModel]:
+        statement = select(ContainerModel).where(ContainerModel.household_id == household_id, ContainerModel.deleted_at.is_(None)).order_by(ContainerModel.name.asc(), ContainerModel.id.asc())
         return self.session.scalars(statement).all()
 
 
@@ -43,19 +58,9 @@ class InventoryRepository:
         return item
 
     def get(self, item_id: str) -> InventoryItemModel | None:
-        statement = select(InventoryItemModel).where(
-            InventoryItemModel.id == item_id,
-            InventoryItemModel.deleted_at.is_(None),
-        )
+        statement = select(InventoryItemModel).where(InventoryItemModel.id == item_id, InventoryItemModel.deleted_at.is_(None))
         return self.session.scalar(statement)
 
     def list_for_household(self, household_id: str) -> Sequence[InventoryItemModel]:
-        statement = (
-            select(InventoryItemModel)
-            .where(
-                InventoryItemModel.household_id == household_id,
-                InventoryItemModel.deleted_at.is_(None),
-            )
-            .order_by(InventoryItemModel.name.asc(), InventoryItemModel.id.asc())
-        )
+        statement = select(InventoryItemModel).where(InventoryItemModel.household_id == household_id, InventoryItemModel.deleted_at.is_(None)).order_by(InventoryItemModel.name.asc(), InventoryItemModel.id.asc())
         return self.session.scalars(statement).all()
