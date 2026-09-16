7352352fb5db8977b25d28061ad490a839da6916
kakelakel <32018143+kakelakel@users.noreply.github.com>
2026-09-16 17:50:54 +0200

    Wire Web polish enhancements

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { initializeEnhancements } from "./enhancements";
 import "./styles.css";
+import "./polish.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+initializeEnhancements();
